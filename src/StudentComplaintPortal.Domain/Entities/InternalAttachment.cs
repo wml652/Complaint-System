1e9bee62a2dc29dab29c67e5c6bd11ddc4cd773e
Muskan Khalil <muskankhalil1246@gmail.com>
2026-08-11 16:45:35 +0500

    Add InternalAttachment entity and DB table for internal-chat attachments (schema only, no logic wired yet)

---
+++ src/StudentComplaintPortal.Domain/Entities/InternalAttachment.cs
@@ -0,0 +1,15 @@
+﻿using StudentComplaintPortal.Domain.Enums;
+
+namespace StudentComplaintPortal.Domain.Entities;
+
+public class InternalAttachment
+{
+    public int Id { get; set; }
+    public int InternalMessageId { get; set; }
+    public required string FileUrl { get; set; }
+    public FileType FileType { get; set; }
+    public long FileSizeBytes { get; set; }
+    public DateTime UploadedAt { get; set; }
+
+    public InternalMessage InternalMessage { get; set; } = null!;
+}
